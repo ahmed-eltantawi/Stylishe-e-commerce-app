923a1ec7337544f58642af8961b5eeeea68c0000
Ahmed Eltantawi <ahmedeltantawi73@gmail.com>
2026-05-23 13:42:14 +0300

    pushing onboarding view

--- lib/features/splash/presentation/splash_view.dart
+++ lib/features/splash/presentation/splash_view.dart
@@ -1,9 +1,28 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:stylish/core/routing/app_routes.dart';
+import 'package:go_router/go_router.dart';
 import 'package:stylish/core/utils/app_colors.dart';
 
-class SplashView extends StatelessWidget {
+class SplashView extends StatefulWidget {
   const SplashView({super.key});
+
+  @override
+  State<SplashView> createState() => _SplashViewState();
+}
+
+class _SplashViewState extends State<SplashView> {
+  Future<void> navigateToOnboarding() async {
+    await Future.delayed(const Duration(seconds: 2), () {
+      context.go(AppRoutes.kOnboardingView);
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    navigateToOnboarding();
+  }
 
   @override
   Widget build(BuildContext context) {
