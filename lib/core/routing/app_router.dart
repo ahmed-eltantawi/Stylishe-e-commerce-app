31340b7963cb63ba6f15ecf4075d7e5a505422d0
Ahmed Eltantawi <ahmedeltantawi73@gmail.com>
2026-05-23 12:57:39 +0300

    init onboarding view

--- lib/core/routing/app_router.dart
+++ lib/core/routing/app_router.dart
@@ -1,13 +1,18 @@
 import 'package:go_router/go_router.dart';
 import 'package:stylish/core/routing/app_routes.dart';
+import 'package:stylish/features/onboarding/presentation/views/onboarding_view.dart';
 import 'package:stylish/features/splash/presentation/splash_view.dart';
 
 abstract final class AppRouter {
   static final router = GoRouter(
     routes: [
       GoRoute(
-        path: AppRoutes.splashView,
+        path: AppRoutes.kSplashView,
         builder: (context, state) => const SplashView(),
+      ),
+      GoRoute(
+        path: AppRoutes.kOnboardingView,
+        builder: (context, state) => const OnboardingView(),
       ),
     ],
   );
