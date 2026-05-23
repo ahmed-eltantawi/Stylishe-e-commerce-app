daaa10c3c5282d50d1281f99b0dc14e2cc2bd779
Ahmed Eltantawi <ahmedeltantawi73@gmail.com>
2026-05-23 16:57:21 +0300

    add gesterdetector on skip word

--- lib/features/onboarding/presentation/widgets/upper_bar_widget.dart
+++ lib/features/onboarding/presentation/widgets/upper_bar_widget.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:go_router/go_router.dart';
+import 'package:stylish/core/routing/app_routes.dart';
 import 'package:stylish/core/utils/app_colors.dart';
 import 'package:stylish/core/utils/app_text_styles.dart';
 
@@ -28,7 +30,10 @@ class UpperBarWidget extends StatelessWidget {
           // spacer
           Spacer(),
           // skip
-          Text("Skip", style: AppTextStyles.semiBold18),
+          GestureDetector(
+            onTap: () => context.go(AppRoutes.kLoginView),
+            child: Text("Skip", style: AppTextStyles.semiBold18),
+          ),
         ],
       ),
     );
